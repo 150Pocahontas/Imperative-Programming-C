ca24b29d5f30b111d7e7cf7714b2ca9188d2c5da
Patrícia Gonçalves Pereira <48392932+150Pocahontas@users.noreply.github.com>
2020-06-05 21:14:29 +0100

    Create Q058_splitQS.c

---
+++ Q058_splitQS.c
@@ -0,0 +1,35 @@
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
+void splitQS (LInt l, int x, LInt *mx, LInt *Mx){
+    *mx = newLInt(1,NULL);
+    *Mx = newLInt(1,NULL);
+    LInt newm = *mx, newM = *Mx;
+    while(l!=NULL){
+        if(l->valor<x){
+            newm->prox = l;
+            l=l->prox;
+            newm = newm->prox;
+            newm->prox = NULL;
+        }
+        else{
+            newM->prox = l;
+            l=l->prox;
+            newM = newM->prox;
+            newM->prox = NULL;
+        }
+        
+    }
+    l = *mx;
+	*mx = (*mx)->prox;
+
+	l = *Mx;
+	*Mx = (*Mx)->prox;
+}
